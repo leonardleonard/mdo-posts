d5fe9d28c61feb80428496fa88ff537d49d73379
leonardleonard <leonardleonard@126.com>
2024-06-15 16:10:17 +0800

    update

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def get_all_posts():
     return jsonify(posts)
 
 
+
 @app.route('/posts/<post_id>', methods=['GET'])
 def get_post(post_id):
     post = collection.find_one({'_id': ObjectId(post_id)})
